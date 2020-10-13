9743b422db422acff9dcd917501b15241112aa15
Robert Morales <rmoralfit@gmail.com>
2020-10-13 00:38:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,17 @@
 function basicTeenager(age) {
-
+if (age === 13-19) {
+  basicTeenager("you are a Teenager!");
+}
 }
 
 function teenager(age) {
-
+if (age === 13-19){
+  teenager("you are a Teenager!")
+} else {
+  teenager("you are not a Teenager!")
 }
+} 
+
 
 function ageChecker(age) {
 
